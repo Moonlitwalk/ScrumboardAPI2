7bbbc99e7fb0163a225e2d64194a3515aaea0be5
Moonlitwalk <165035940+Moonlitwalk@users.noreply.github.com>
2026-03-12 11:29:46 +0000

    wip

--- routes/api.php
+++ routes/api.php
@@ -6,13 +6,9 @@ use App\Models\Scrumboard;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/create', [ScrumboardController::class, 'store']);
-
 Route::get('/boards', [ScrumboardController::class, 'index']);
-
 Route::get('/boards/{id}', [ScrumboardController::class, 'show']);
-
 Route::put('/boards/{scrumboard}', [ScrumboardController::class, 'update']);
-
 Route::delete('/boards/{scrumboard}', [ScrumboardController::class, 'destroy']);
 
 
